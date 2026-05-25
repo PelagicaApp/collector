c7e3736b5663c9f894cd98cb90ed4fc2e1ef8d32
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-05-25 16:08:24 +0200

    Add proxy setting

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"pelagica-collector/db"
 	"pelagica-collector/handlers"
 
 	"github.com/gofiber/fiber/v3"
 )
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4321"
+	}
+	return ":" + port
+}
 
 func main() {
 	ctx := context.Background()
@@ -24,10 +33,19 @@ func main() {
 
 	h := handlers.NewHandler(database)
 
-	app := fiber.New()
+	fiberCfg := fiber.Config{}
+	if os.Getenv("BEHIND_PROXY") == "true" {
+		fiberCfg.ProxyHeader = fiber.HeaderXForwardedFor
+		fiberCfg.TrustProxy = true
+		fiberCfg.TrustProxyConfig = fiber.TrustProxyConfig{
+			Proxies: []string{"127.0.0.1", "::1"},
+		}
+	}
+
+	app := fiber.New(fiberCfg)
 
 	app.Post("/ping", h.Ping)
 	app.Get("/stats", h.Stats)
 
-	log.Fatal(app.Listen(":4000"))
+	log.Fatal(app.Listen(getPort()))
 }
